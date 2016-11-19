0186650aee79b3e20c743f1b7ed94098a7e2f6a0
SteveQ <sawicki.adam172@gmail.com>
2016-11-19 19:12:59 +0100

    Logging page layout

--- app/src/main/java/com/steveq/cashcontrol/ui/activities/SplashActivity.java
+++ app/src/main/java/com/steveq/cashcontrol/ui/activities/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.steveq.cashcontrol.ui.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends Activity {
@@ -8,5 +9,12 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        goLogging();
+    }
+
+    private void goLogging() {
+        Intent intent = new Intent(this, LoggingActivity.class);
+        startActivity(intent);
     }
 }
