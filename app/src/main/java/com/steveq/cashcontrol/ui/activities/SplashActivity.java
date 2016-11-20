aff2dee78ce8e616afec9d9f595033406ce185ae
SteveQ <sawicki.adam172@gmail.com>
2016-11-20 20:13:04 +0100

    Creating database on splash screen

--- app/src/main/java/com/steveq/cashcontrol/ui/activities/SplashActivity.java
+++ app/src/main/java/com/steveq/cashcontrol/ui/activities/SplashActivity.java
@@ -2,7 +2,10 @@ package com.steveq.cashcontrol.ui.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteException;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.steveq.cashcontrol.database.ReceiptDataBaseHelper;
 
@@ -13,14 +16,40 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        rdbHelper = new ReceiptDataBaseHelper(this);
-        rdbHelper.getReadableDatabase();
-        rdbHelper.close();
-        goLogging();
+        new dbInitializationAsyncTask().execute();
     }
 
     private void goLogging() {
         Intent intent = new Intent(this, LoggingActivity.class);
         startActivity(intent);
+    }
+
+
+    private class dbInitializationAsyncTask extends AsyncTask<Void, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            ReceiptDataBaseHelper rdbHelper = new ReceiptDataBaseHelper(SplashActivity.this);
+            try {
+                rdbHelper.getReadableDatabase();
+                rdbHelper.close();
+                return true;
+            } catch(SQLiteException sqle){
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+
+            if(!aBoolean){
+                goLogging();
+                Toast.makeText(SplashActivity.this, "Cannot connect to database", Toast.LENGTH_LONG).show();
+            }else{
+                goLogging();
+                Toast.makeText(SplashActivity.this, "database created", Toast.LENGTH_LONG).show();
+            }
+
+        }
     }
 }
