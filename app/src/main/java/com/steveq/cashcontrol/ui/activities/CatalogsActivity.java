83525e2eb77a8267e43f91ab55798f63e954ba15
SteveQ <sawicki.adam172@gmail.com>
2016-11-24 22:24:14 +0100

    Make AppCompat consistent with material style

--- app/src/main/java/com/steveq/cashcontrol/ui/activities/CatalogsActivity.java
+++ app/src/main/java/com/steveq/cashcontrol/ui/activities/CatalogsActivity.java
@@ -1,9 +1,12 @@
 package com.steveq.cashcontrol.ui.activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,32 +20,25 @@ import java.util.ArrayList;
 public class CatalogsActivity extends AppCompatActivity {
 
     Toolbar catalogsToolbar;
-    Button create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalogs);
+        setToolbarView();
+        getWindow().
+                getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
 
-        create = (Button) findViewById(R.id.create);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.catalogs_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    private void setToolbarView(){
         catalogsToolbar = (Toolbar) findViewById(R.id.catalogsToolbar);
         setSupportActionBar(catalogsToolbar);
-
-        new CatalogsDataSource(CatalogsActivity.this).createCatalog(new Catalog(-1, UserManager.mCurrentUser.getId(), 0, "test", 10, 10));
-        new CatalogsDataSource(CatalogsActivity.this).createCatalog(new Catalog(-1, UserManager.mCurrentUser.getId(), 0, "test2", 20, 20));
-
-        create.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ArrayList<Catalog> ca = new CatalogsDataSource(CatalogsActivity.this).readCatalogs();
-                new CatalogsDataSource(CatalogsActivity.this).deleteCatalog(ca.get(1));
-                new CatalogsDataSource(CatalogsActivity.this).updateCatalogSum(ca.get(0), 12.23);
-                ArrayList<Catalog> ca2 = new CatalogsDataSource(CatalogsActivity.this).readCatalogs();
-                for(Catalog c : ca2){
-                    Log.d("CC", Double.toString(c.getSum()));
-                }
-            }
-        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
